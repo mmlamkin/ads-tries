185904d90d019d409d7d599f4fe342dd23f01939
Mary Lamkin <mlamkin@indeed.com>
2020-06-22 20:18:19 -0700

    committing

--- src/data_structures/trie.js
+++ src/data_structures/trie.js
@@ -11,17 +11,47 @@ class Trie {
     this.buildCode = buildCode;
     this._root = new this.Node();
     this._count = 0;
-    words.forEach(word => this.addWord(word));
+    const uniqueWords = words.filter((v, i, a) => a.indexOf(v) === i);
+    uniqueWords.forEach(word => this.addWord(word));
   }
 
   addWord(word) {
     const code = this.buildCode(word);
+    let node = this._root;
+    debugger;
+
+    for (let c of code) {
+      if (!node.children[c]) {
+        node.children[c] = new TrieNode();
+      }
+        node = node.children[c];
+        if (!node.words.includes(word)) {
+          node.words.push(word);
+        }
+    }
+    this._count += 1;
   }
 
   lookupCode(code) {
+    let node = this._root;
+    for (let c of code) {
+      node = node.children[c]
+      if (!node) {
+        return [];
+      }
+    }
+    return node.words
   }
 
   lookupPrefix(codePrefix) {
+    let node = this._root;
+    for (let c of codePrefix) {
+      node = node.children[c]
+      if (!node) {
+        return [];
+      }
+    }
+    return node.words
   }
 
   count() {
